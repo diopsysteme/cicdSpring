ad4746b227c1531be2d4a0ade3a31bf678c8f356
bakemono-san <mousdef34@gmail.com>
2024-09-20 19:25:05 +0000

    first commit

---
+++ src/main/java/java/SchoolAppJavaApplication.java
@@ -0,0 +1,13 @@
+package java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAppJavaApplication.class, args);
+	}
+
+}
