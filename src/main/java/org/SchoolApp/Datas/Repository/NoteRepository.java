41d204422ca39f3a601545cb81c566ecf89bd1d4
bakemono-san <mousdef34@gmail.com>
2024-09-21 19:44:29 +0000

    update( added some repos and enum)

--- src/main/java/org/SchoolApp/Datas/Repository/NoteRepository.java
+++ src/main/java/org/SchoolApp/Datas/Repository/NoteRepository.java
@@ -1,7 +1,12 @@
 package org.SchoolApp.Datas.Repository;
 
+import org.SchoolApp.Datas.Entity.ApprenantEntity;
 import org.SchoolApp.Datas.Entity.NotesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends SoftDeleteRepository<NotesEntity, Long> {
+    List<NotesEntity> findByApprenant(ApprenantEntity apprenant);
+    List<NotesEntity> findByApprenantAndModule(ApprenantEntity apprenant, Module module);
 }
