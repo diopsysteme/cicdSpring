124da994b9169836f3ef89ebf783df4a8ef02a5f
bakemono-san <mousdef34@gmail.com>
2024-09-21 18:25:25 +0000

    update( added softDeleteRepository)

--- src/main/java/org/SchoolApp/Datas/Repository/NoteRepository.java
+++ src/main/java/org/SchoolApp/Datas/Repository/NoteRepository.java
@@ -3,5 +3,5 @@ package org.SchoolApp.Datas.Repository;
 import org.SchoolApp.Datas.Entity.NotesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository extends JpaRepository<NotesEntity, Long> {
+public interface NoteRepository extends SoftDeleteRepository<NotesEntity, Long> {
 }
--- src/main/java/org/SchoolApp/Datas/Repository/ReferentielRepository.java
+++ src/main/java/org/SchoolApp/Datas/Repository/ReferentielRepository.java
@@ -3,5 +3,5 @@ package org.SchoolApp.Datas.Repository;
 import org.SchoolApp.Datas.Entity.ReferentielEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReferentielRepository extends JpaRepository<ReferentielEntity,Long> {
+public interface ReferentielRepository extends SoftDeleteRepository<ReferentielEntity,Long> {
 }
