7fe65224ed2b88cba6dc2a3ee78fbca68ba4485c
Cheikh <cheeikhmbacke@gmail.com>
2024-09-22 02:26:41 +0000

    repository et services | fonction | modules | referentiel v-3

--- src/main/java/org/SchoolApp/SchoolAppJavaApplication.java
+++ src/main/java/org/SchoolApp/SchoolAppJavaApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.SchoolApp.Datas.Repository")
 @EntityScan(basePackages = "org.SchoolApp.Datas.Entity")
-public class SchoolAppJavaApplication {
+public class SchoolAppJavaApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolAppJavaApplication.class, args);
 	}
+
 
 }
