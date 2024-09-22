fd525d4b36718823c7a91b414573e376cfda48d4
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-09-22 12:55:15 +0000

    User and Role Repository

--- src/main/java/org/SchoolApp/SchoolAppJavaApplication.java
+++ src/main/java/org/SchoolApp/SchoolAppJavaApplication.java
@@ -1,6 +1,7 @@
 package org.SchoolApp;
 
 import org.SchoolApp.Services.Impl.NoteService;
+import org.SchoolApp.Services.Impl.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,8 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.SchoolApp.Datas.Repository")
 @EntityScan(basePackages = "org.SchoolApp.Datas.Entity")
-public class SchoolAppJavaApplication {
 
+public class SchoolAppJavaApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SchoolAppJavaApplication.class, args);
@@ -19,6 +20,10 @@ public class SchoolAppJavaApplication {
 		NoteService noteService = (NoteService) context.getBean(NoteService.class);
 
 		noteService.getAllNotes();
+
+		UserService userService = (UserService) context.getBean(UserService.class);
+
+		userService.getAllUsersWithDetails();
 	}
 
 }
